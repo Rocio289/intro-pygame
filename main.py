4dd02ff8063e1aeb06d521efc5c166ba428f7aa6
DESKTOP-RPQLQA0\Rocío <rocio2ms89@gmail.com>
2025-07-16 18:41:21 +0200

    primer commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame as pg
+
+
+#inicializar todos los modulos de pygame (pantallas, obejetos, eventos, etc)
+pg.init()
+
+
+#crear pantalla o surface
+pantalla = pg.display.set_mode( (800,600) )#definicion de tamaño de pantalla
+pg.display.set_caption("Intro Pygame")#agregar un titulo a mi ventana
+
+game_over = True
+x = 0
+vx = 1
+while game_over:
+    for eventos in pg.event.get():#capturar todos los eventos mientras se ejecuta el bucle
+        print(eventos)
+        if eventos.type == pg.QUIT:
+            game_over = False
+    pantalla.fill( (50, 189, 172) )#asignar un color a la pantalla
+    #agregamor objeto a la pantalla
+    x += vx
+    if x == 800 or x == 0:
+        vx = vx*-1
+
+    
+    #draw.rect(sourface, color en (rgb), posiciones(posicionX, posicionY, tamañoX, tamañoY))
+    pg.draw.rect(pantalla, (227, 58, 31), (x, 300-20, 20, 20))
+
+    pg.display.flip()#funcion para cargar toda la configuracion que va dentro de la pantalla
+
+pg.quit()
